da1f5e377be1ba9f4f871aa92ed214739bc526ec
Marcin Nowaczewski <nowaczewskimarcin@gmail.com>
2022-04-05 14:16:54 +0200

    init

---
+++ src/pages/GithubRanking.vue
@@ -0,0 +1,90 @@
+<!-- <template>
+<div class="fit row wrap justify-start items-start content-start" style="margin-top: 50px; padding: 10px;">
+<div class="element">
+
+    <div class="q-pa-md q-gutter-sm">
+   <q-banner rounded class="bg-indigo-10 text-white">
+
+      Click to see <strong>Top 10 Users </strong>  on Github
+
+      <template v-slot:action>
+          <q-btn color="green-6" @click="topUsers"  icon-right="send" label="Top 10 Users" style="width:250px"/>
+      </template>
+    </q-banner>
+  </div>
+
+<div class="q-pa-md q-gutter-sm">
+    <q-banner rounded class="bg-indigo-10 text-white">
+
+      Click to see <strong>Top 10 Organizations </strong> on Github
+
+      <template v-slot:action>
+          <q-btn color="green-6"  icon-right="send" label="Top 10 Organizations" style="width:250px"  />
+      </template>
+    </q-banner>
+  </div>
+
+<div class="q-pa-md q-gutter-sm"> 
+  <q-banner rounded class="bg-indigo-10 text-white">
+
+      Click to see <strong>Top 10 Repositories </strong>on Github
+
+      <template v-slot:action>
+          <q-btn color="green-6
+"  icon-right="send" style="width:250px" label="Top 10 Repositories" />
+      </template>
+    </q-banner>
+  </div>
+
+
+</div>
+
+
+<div class="element">jjjj
+
+</div>
+
+</div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'GithubRanking',
+  // methods: {
+  //   getReposOver() {
+  //     const url = "https://api.github.com/search/repositories?q=stars:>100000";
+  //     response = await fetch(url);
+  //     result = this.response.json();
+  //   }
+  // }
+  data() {
+    return {
+      user: "",
+      inputUser: "",
+      hide: false,
+      HideBox: false,
+
+
+    };
+  },
+  methods: {
+    topUsers() {
+      const link = `https://api.github.com/users/${this.inputUser}`;
+      axios.get(link).then((response) => {
+        this.user = response.data;
+    
+        // console.log(response.data)
+       
+      });
+    },
+  },
+});
+</script>
+
+<style scoped>
+.element {
+    overflow: auto; min-width: 50%; max-width: 50%;
+}
+</style> -->
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="flex flex-center" style="background: linear-gradient(90deg, rgba(2,0,36,1) 3%, rgba(8,8,108,1) 18%, rgba(9,9,121,1) 85%, rgba(158,0,255,1) 117%); color: white;
+">
+  <div class="element">
+  <h2 style="color: white;
+  font-weight: 900">Welcome to the Github clone!</h2>
+        <h5 style="color: #8193b2; font-weight: 900" >In this page you can find users repositories, find directly user, read info about chosen one user and see Github rankings.</h5>
+ 
+
+
+  </div>
+  <div class="element">
+
+<img src="https://logodix.com/logo/64439.png" width="380" height="380" />
+    
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+
+<style scoped>
+
+.element {
+  display: inline-block;
+  width: 600px;
+  height: 400px;
+  padding: 15px;
+  margin: 20px auto;
+  border-color: azure;
+  border-style: dotted;
+}
+
+.center {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+ }
+</style>
