3ed87abbd46c0cb1b6ebba1d37c72657facb56e7
Marcin Nowaczewski <nowaczewskimarcin@gmail.com>
2022-04-08 10:00:33 +0200

    computed property

--- src/pages/FinderV3.vue
+++ src/pages/FinderV3.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container relative" style="padding: 5%; margin-top: 10px;">
-        <q-select v-model="selectedValue" :options="options" label="Standout" />
+        <q-select v-model="selectedValue" :options="optionsOnlyLogin" label="Standout" />
     </div>
 </template>
 
@@ -11,12 +11,22 @@ export default defineComponent({
     name: 'FinderV3',
     data() {
         return {
-            options: ["kkkk", "aaaa"],
-            selectedValue: "kkkkkk",
+            options: [
+                { login: "marcin", avatar_url: "https://avatars.githubusercontent.com/u/13852170?v=4" },
+                { login: "nie marcin", avatar_url: "https://avatars.githubusercontent.com/u/3524577?v=4" }
+            ],
+            selectedValue: null,
         };
     },
     methods: {
 
+
+    },
+
+    computed: {
+        optionsOnlyLogin() {
+            return this.options.map((x) => { return x.login });
+        }
     }
 
 })
