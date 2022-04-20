e644f35ffeab56f3b72fa31d76bee8ae1fe3243c
Marcin Nowaczewski <nowaczewskimarcin@gmail.com>
2022-04-20 11:10:03 +0200

    add more icons to quasar

--- quasar.config.js
+++ quasar.config.js
@@ -23,7 +23,7 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
     boot: [
-      
+
       'axios',
     ],
 
@@ -34,12 +34,12 @@ module.exports = configure(function (ctx) {
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
-      // 'ionicons-v4',
-      // 'mdi-v5',
-      // 'fontawesome-v6',
-      // 'eva-icons',
-      // 'themify',
-      // 'line-awesome',
+      'ionicons-v4',
+      'mdi-v5',
+      'fontawesome-v6',
+      'eva-icons',
+      'themify',
+      'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
@@ -69,9 +69,9 @@ module.exports = configure(function (ctx) {
 
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
-      chainWebpack (/* chain */) {}
-      
+
+      chainWebpack(/* chain */) { }
+
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
@@ -113,14 +113,14 @@ module.exports = configure(function (ctx) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       maxAge: 1000 * 60 * 60 * 24 * 30,
-        // Tell browser when a file from the server should expire from cache (in ms)
+      // Tell browser when a file from the server should expire from cache (in ms)
 
-      
-      chainWebpackWebserver (/* chain */) {},
-      
+
+      chainWebpackWebserver(/* chain */) { },
+
 
       middlewares: [
         ctx.prod ? 'compression' : '',
@@ -135,9 +135,9 @@ module.exports = configure(function (ctx) {
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
       // if using workbox in InjectManifest mode
-      
-      chainWebpackCustomSW (/* chain */) {},
-      
+
+      chainWebpackCustomSW(/* chain */) { },
+
 
       manifest: {
         name: `github-clone4`,
@@ -211,13 +211,13 @@ module.exports = configure(function (ctx) {
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
-      chainWebpackMain (/* chain */) {},
-      
 
-      
-      chainWebpackPreload (/* chain */) {},
-      
+      chainWebpackMain(/* chain */) { },
+
+
+
+      chainWebpackPreload(/* chain */) { },
+
     }
   }
 });
