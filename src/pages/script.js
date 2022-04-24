1dde972a1f9310de90f862e7c30c02200cd36a2c
Marcin Nowaczewski <nowaczewskimarcin@gmail.com>
2022-04-24 20:26:09 +0200

    update script

--- src/pages/script.js
+++ src/pages/script.js
@@ -5,20 +5,20 @@ const icon = searchWrapper.querySelector(".icon");
 let linkTag = searchWrapper.querySelector("a");
 let webLink;
 // if user press any key and release
-inputBox.onkeyup = (e)=>{
+inputBox.onkeyup = (e) => {
     let userData = e.target.value; //user enetered data
     let emptyArray = [];
-    if(userData){
-        icon.onclick = ()=>{
+    if (userData) {
+        icon.onclick = () => {
             webLink = `https://www.google.com/search?q=${userData}`;
             linkTag.setAttribute("href", webLink);
             linkTag.click();
         }
-        emptyArray = suggestions.filter((data)=>{
+        emptyArray = suggestions.filter((data) => {
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
         });
-        emptyArray = emptyArray.map((data)=>{
+        emptyArray = emptyArray.map((data) => {
             // passing return data inside li tag
             return data = `<li>${data}</li>`;
         });
@@ -29,27 +29,27 @@ inputBox.onkeyup = (e)=>{
             //adding onclick attribute in all li tag
             allList[i].setAttribute("onclick", "select(this)");
         }
-    }else{
+    } else {
         searchWrapper.classList.remove("active"); //hide autocomplete box
     }
 }
-function select(element){
+function select(element) {
     let selectData = element.textContent;
     inputBox.value = selectData;
-    icon.onclick = ()=>{
+    icon.onclick = () => {
         webLink = `https://www.google.com/search?q=${selectData}`;
         linkTag.setAttribute("href", webLink);
         linkTag.click();
     }
     searchWrapper.classList.remove("active");
 }
-function showSuggestions(list){
+function showSuggestions(list) {
     let listData;
-    if(!list.length){
+    if (!list.length) {
         userValue = inputBox.value;
         listData = `<li>${userValue}</li>`;
-    }else{
-      listData = list.join('');
+    } else {
+        listData = list.join('');
     }
     suggBox.innerHTML = listData;
 }
